c9106003e7c2a343c41df148cd36463a4a35211b
Igor Kramskoy <ig.kramskoy@it-justice.com>
2022-08-05 12:41:54 +0300

    refactor CharBar.

--- src/Components/Pages/Main/CharBar/CharBar.tsx
+++ src/Components/Pages/Main/CharBar/CharBar.tsx
@@ -1,42 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Echarts from 'echarts-for-react';
 
 import { Box } from '@mui/material';
 
-import { useAppSelector } from '../../../../app/hooks';
+import {useChartBar} from "./Charbar.utils";
 
 export const ChartBar = () => {
-  const [dataValueBears, setDataValueBears] = useState<number[]>([]);
-  const [dataBears, setDataBears] = useState<string[]>([]);
+  const {dataBears, options} =useChartBar();
 
-  const {beers} = useAppSelector(state => state.beer);
-
-  const options = {
-      xAxis: {
-        type: 'category',
-        data: dataBears
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: dataValueBears,
-          type: 'bar'
-        }
-      ]
-  };
-  useEffect(() => {
-    const newDataBeer = beers.slice(10, 16);
-    const dataValueChart: number[] = newDataBeer.map((item) => {
-      return item.ibu
-    })
-    const dataChart: string[]= newDataBeer.map((item) => {
-      return item.name
-    })
-    setDataBears(dataChart)
-    setDataValueBears(dataValueChart)
-  }, [beers])
   return (
     <Box>
       {dataBears.length > 0 && <Echarts option={options} style={{ height: '600px'}}/>}
